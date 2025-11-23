170c52b67c73c7fb1965c6db22535ebdd58e74c2
Ryan <119005046+CodeByDex@users.noreply.github.com>
2025-11-23 10:05:27 -0500

    updated layout and added opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,35 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Bydex - Web Development & Pottery",
+  title: "ByDex",
   description: "Professional portfolio showcasing web development and pottery work",
+  keywords: [
+    "ByDex", "Made By Dex", "Dex", "Ryan Spath", "Findlay Ohio", "Web Development",
+    "Pottery", "Knitting", "Fiber Arts", "Sewing", "LGBTQ+"
+  ],
+  authors: [{ name: "Ryan Spath", url: "https://github.com/CodeByDex" }],
+  alternates: {
+    languages: {
+      "en-US": "https://bydex.co",
+    },
+  },
+  openGraph: {
+    title: "ByDex",
+    description: "Professional portfolio showcasing web development and pottery work",
+    url: "https://bydex.co",
+    siteName: "ByDex",
+    images: [
+      {
+        url: "/branding/Makers Mark.png",
+        width: 800,
+        height: 600,
+        alt: "ByDex Logo",
+      },
+      ]
+  },
+  icons: {
+    icon: "/branding/Makers Mark Icon.png",
+  },
   metadataBase: new URL("https://bydex.co"),
 };
 
